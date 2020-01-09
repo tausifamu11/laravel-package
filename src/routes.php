3529c0e2c938eea0df816d3ae41edf52dfafad24
tausifamu11 <32100355+tausifamu11@users.noreply.github.com>
2020-01-09 15:35:45 +0530

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,5 @@
 Route::get('calculator', function(){
     echo 'Hello from the calculator package!';
 });
-Route::get('add/{a}/{b}', 'Devdojo\Calculator\CalculatorController@add');
-Route::get('subtract/{a}/{b}', 'Devdojo\Calculator\CalculatorController@subtract');
+Route::get('add/{a}/{b}', 'Devd\Calculator\CalculatorController@add');
+Route::get('subtract/{a}/{b}', 'Devd\Calculator\CalculatorController@subtract');
